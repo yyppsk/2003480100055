dee63058b4954b2441e0fd4b23128ac17ac0debe
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-10-06 11:03:26 +0530

    register

--- server.js
+++ server.js
@@ -1,26 +1,63 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const { v4: uuidv4 } = require("uuid");
 
 app.use(express.json());
 
 const authToken = "12345";
 
-const mockTrainData = [];
+const fs = require("fs");
 
-const fetchTrainData = () => {
-  return mockTrainData;
-};
+// Path to the JSON data file
+const dataFilePath = "./companies.json";
 
-const authenticate = (req, res, next) => {
-  const userAuthToken = req.headers.authorization;
+app.post("/train/register", (req, res) => {
+  try {
+    const { companyName, ownerName, rollNo, ownerEmail, accessCode } = req.body;
 
-  if (!userAuthToken || userAuthToken !== authToken) {
-    return res.status(401).json({ message: "Authentication failed" });
+    // Load existing data from the JSON file
+    const companiesData = JSON.parse(fs.readFileSync(dataFilePath, "utf8"));
+
+    // Check if the company with the same rollNo already exists
+    const existingCompany = companiesData.find(
+      (company) => company.rollNo === rollNo
+    );
+
+    if (existingCompany) {
+      return res.status(400).json({ error: "Company already registered" });
+    }
+
+    // Assuming you have a function to generate random client ID and client secret
+    const clientId = generateRandomClientId();
+    const clientSecret = generateRandomClientSecret();
+
+    // Create a new company object
+    const newCompany = {
+      companyName,
+      ownerName,
+      rollNo,
+      ownerEmail,
+      accessCode,
+      clientId,
+      clientSecret,
+    };
+
+    // Add the new company to the data array
+    companiesData.push(newCompany);
+
+    // Write the updated data back to the JSON file
+    fs.writeFileSync(dataFilePath, JSON.stringify(companiesData, null, 2));
+
+    res.status(200).json({
+      companyName,
+      clientId,
+      clientSecret,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
-
-  next();
-};
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
