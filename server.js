cd4eeca8e2f9f7473a4844e467347b3ca5e7b7b3
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-10-06 11:04:00 +0530

    Check for Register

--- server.js
+++ server.js
@@ -9,17 +9,14 @@ const authToken = "12345";
 
 const fs = require("fs");
 
-// Path to the JSON data file
 const dataFilePath = "./companies.json";
 
 app.post("/train/register", (req, res) => {
   try {
     const { companyName, ownerName, rollNo, ownerEmail, accessCode } = req.body;
 
-    // Load existing data from the JSON file
     const companiesData = JSON.parse(fs.readFileSync(dataFilePath, "utf8"));
 
-    // Check if the company with the same rollNo already exists
     const existingCompany = companiesData.find(
       (company) => company.rollNo === rollNo
     );
@@ -28,11 +25,9 @@ app.post("/train/register", (req, res) => {
       return res.status(400).json({ error: "Company already registered" });
     }
 
-    // Assuming you have a function to generate random client ID and client secret
     const clientId = generateRandomClientId();
     const clientSecret = generateRandomClientSecret();
 
-    // Create a new company object
     const newCompany = {
       companyName,
       ownerName,
@@ -42,11 +37,8 @@ app.post("/train/register", (req, res) => {
       clientId,
       clientSecret,
     };
-
-    // Add the new company to the data array
     companiesData.push(newCompany);
 
-    // Write the updated data back to the JSON file
     fs.writeFileSync(dataFilePath, JSON.stringify(companiesData, null, 2));
 
     res.status(200).json({
